0a40ecc0c2a04922243b902a3cf86141756aad2a
Wudnkidner <6326236java@gmail.com>
2016-08-25 16:13:21 +0300

    Version 1.1 (Work)

--- src/main/java/W5CreateFightStage.java
+++ src/main/java/W5CreateFightStage.java
@@ -1,3 +1,4 @@
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,6 +12,8 @@ public class W5CreateFightStage {
 	private static ComboBox tournamentCBox;
 	private static ComboBox placeCBox;
 	private static ComboBox dateCBox;
+	private static ComboBox fightNumCBox;
+	private static ComboBox weightCBox;
 	private static ComboBox fighterRedCBox;
 	private static ComboBox fighterBlueCBox;
 	private static ComboBox countryRedCBox;
@@ -18,7 +21,7 @@ public class W5CreateFightStage {
 	private static ComboBox judge1CBox;
 	private static ComboBox judge2CBox;
 	private static ComboBox judge3CBox;
-	private static ComboBox RefereeCBox;
+	private static ComboBox refereeCBox;
 
 	public static void setCreateFightsStage (Stage stage) {
 
@@ -26,11 +29,15 @@ public class W5CreateFightStage {
 		gp.setPrefSize(1024, 768);
 		gp.setVgap(5);
 		gp.setHgap(10);
-		gp.setAlignment(Pos.CENTER);
+		gp.setPadding(new Insets(10.0));
+		gp.setAlignment(Pos.TOP_CENTER);
 		gp.addRow(0, new Label("Event name:"), W5CreateFightStage.createTournamentCBox());
 		gp.addRow(1, new Label("Place:") ,W5CreateFightStage.createPlaceCBox(),new Label("Date:"), W5CreateFightStage.createDateCBox());
-		gp.addRow(2, new Label("Fight number"), new Label("Weight:"));
-
+		gp.addRow(2, new Label("Fight number:"), createFightNumCBox(), new Label("Weight:"), createWeightCBox());
+		gp.addRow(3, new Label("Fighter red:"), createFighterRedCBox(), new Label("Fighter blue:"), createFighterBlueCBox());
+		gp.addRow(4, new Label("Country red:"), createCountryRedCBox(), new Label("Country blue:"), createCountryBlueCBox());
+		gp.addRow(5, new Label("First judge:"), createJudge1CBox(), new Label("Second judge:"),createJudge2CBox(), new Label("Third judge: "), createJudge3CBox());
+		gp.addRow(6, new Label("Referee:"), createRefereeCBox());
 
 		Scene scene = new Scene(gp);
 		stage.setScene(scene);
@@ -55,6 +62,18 @@ public class W5CreateFightStage {
 		dateCBox = new ComboBox();
 
 		return dateCBox;
+	}
+
+	private static ComboBox createFightNumCBox () {
+		fightNumCBox = new ComboBox();
+
+		return fightNumCBox;
+	}
+
+	private static ComboBox createWeightCBox () {
+		weightCBox = new ComboBox();
+
+		return weightCBox;
 	}
 
 	private static ComboBox createFighterRedCBox () {
@@ -97,6 +116,12 @@ public class W5CreateFightStage {
 		fighterRedCBox = new ComboBox();
 
 		return fighterRedCBox;
+	}
+
+	private static ComboBox createRefereeCBox () {
+		refereeCBox = new ComboBox();
+
+		return refereeCBox;
 	}
 
 }
